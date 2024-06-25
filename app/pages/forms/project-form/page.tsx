69a500aba484347db510b45438e5ea5b10612a33
benneoh <benneoh@ssw.com.au>
2024-06-26 00:31:50 +1000

    Implement form for add projects

--- app/pages/forms/project-form/page.tsx
+++ app/pages/forms/project-form/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import TextField from '@mui/material/TextField'
-import React from 'react'
-import { useForm, SubmitHandler } from "react-hook-form"
-import Button from '@mui/material/Button';
-import { FormControl, FormGroup, FormLabel } from '@mui/material';
 import Textarea from '@mui/joy/Textarea';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { redirect } from 'next/navigation';
+import { useForm } from "react-hook-form";
 
 type Inputs = {
   projectName: string
@@ -17,9 +16,31 @@ type Inputs = {
 const projectForm = () => {
     const {register, handleSubmit} = useForm();
 
-    const onSubmit = (data : any) => {
-        console.log('Form data', data)
-    }
+    const onSubmit = async (data : any) => {
+        try {
+            const response = await fetch('/api/add-projects', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name: data.projectName,
+                companyName: data.companyName,
+                budget: data.budget,
+                description: data.description,
+              }),
+            });
+      
+            if (response.ok) {
+              const result = await response.json();
+              console.log('Project created successfully', result);
+              redirect('/')
+            } else {
+              console.error('Failed to create project', await response.json());
+            }
+          } catch (error) {
+            console.error('Error creating project', error);
+          }    }
 
     return (
         <>
@@ -55,6 +76,7 @@ const projectForm = () => {
                         required
                         fullWidth
                 />
+                {/* {"<script>alert('Hello! I am an alert box!');</script>"}} */}
                 <Textarea 
                     {...register('description')}
                     minRows={2} 
